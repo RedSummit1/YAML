40b302098be447081a49ee56df9c5375bd1391a8
Jonathan Shields <jshieldsf@gmail.com>
2024-02-17 02:24:05 -0500

    Modify python libaray files?

--- Python/VIRTUAL_ENVIRONMENT/DiscordBot/test.py
+++ Python/VIRTUAL_ENVIRONMENT/DiscordBot/test.py
@@ -1,9 +1,20 @@
+#!../bin/python3
+
 from openai import OpenAI 
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
-model = OpenAI(api_key=config["KEY"])
+client = OpenAI(api_key=config["KEY"])
 
+completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role":"system", "content":"You are a helpful assistant."},
+        {"role":"user","content":"Hello!"}
+    ]
+)
+
+print(completion.choices[0].message.content)
 
 
 
