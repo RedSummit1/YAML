f033bce53233911fdcc151026ca97e15e4e64dbe
Jonathan Shields <jshieldsf@gmail.com>
2024-03-03 04:47:09 -0500

    Add memory queue and remove possibility of stream response

--- Code/VIRTUAL_ENVIRONMENT/Python/OpenAi.py
+++ Code/VIRTUAL_ENVIRONMENT/Python/OpenAi.py
@@ -1,4 +1,4 @@
-#!../bin/python3
+#../bin/python3
 from openai import OpenAI 
 from dotenv import dotenv_values
 
@@ -19,27 +19,23 @@ class Chat(OpenAI):
     def read(self,*prompt):
         response = ""
         Chat.cycle({"role":"user","content": " ".join(prompt)})
-
-
-
-
         output = self.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
-                {"role":"system", "content":"You are a helpful assistant."},
+                {"role":"system", "content":"You're seething with anger!!"},
                 *Chat.queue
                 #{"role":"user","content":" ".join(prompt)}
             ],
-            stream=True,
         )
-        for chunk in output:
-            block = chunk.choices[0].delta.content
-            print(block if block else "\n",end="",flush=True)
-            response += block if block else ""
+        #for chunk in output:
+        #    block = chunk.choices[0].delta.content
+        #    if block != " ":
+        #        yield(block if block else "\n")
+        #        #,end="",flush=True)
+        #    response += block if block else ""
 
-        Chat.cycle({"role":"assistant","content":response})
-
-       # .choices[0].message.content)
+        Chat.cycle({"role":"assistant","content":output.choices[0].message.content})
+        return Chat.queue[-1]["content"]
    
     
     
--- Code/VIRTUAL_ENVIRONMENT/Python/clientbot.py
+++ Code/VIRTUAL_ENVIRONMENT/Python/clientbot.py
@@ -1,6 +1,7 @@
 import nextcord
 from nextcord import Interaction
 from nextcord.ext import commands
+from OpenAi import Chat
 
 intents = nextcord.Intents.default()
 intents.members = True
@@ -8,11 +9,15 @@ intents.message_content = True
 
 client = commands.Bot(command_prefix = "!", intents=intents)
 
+
+gpt = Chat()
+
+
 @client.event
 async def on_ready():
    print("The bot is now ready for use") 
 
-testingServerID = 1200426642733027428
+testingServerID = 1197063979298725958
 
 
 # This is the example slash command
@@ -22,12 +27,16 @@ testingServerID = 1200426642733027428
 async def helloo(interaction: Interaction,text:str):
     await interaction.response.send_message("Hello I am ready to do whatever you want me to do. I got this {}".format(text))
 
+@client.slash_command(name = "chat",description="Chat GPT in discord", guild_ids=[testingServerID]) # name has to be lowercase
+async def chat(interaction: Interaction,text:str):
+    await interaction.response.send_message(gpt.read(text))
+
 #---------------------------------------------------------------------------------------------------------------------------------
 
 
 
 
-client.run('MTIxMzY5NzQ0MDE0NjkxNTQyOA.GvIC2S.dtK_QxOyBN-2xDisocjXuv5AusY38JYNDo1T90')
+client.run('MTIwNDk5NDg4OTkyMjY0NjA2Nw.G1BAFJ.S6e7KCsEH9hQvGvvqhWw0BWK6lhWeuHuLC66U8')
 
 
 
