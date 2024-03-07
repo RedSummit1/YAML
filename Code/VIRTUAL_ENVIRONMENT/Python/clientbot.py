4541e6ccebec871f852732b8d28fc860734f4bda
Jonathan Shields <jshieldsf@gmail.com>
2024-03-07 12:53:18 -0500

    Add draw functionality to bot

--- Code/VIRTUAL_ENVIRONMENT/Python/clientbot.py
+++ Code/VIRTUAL_ENVIRONMENT/Python/clientbot.py
@@ -13,48 +13,32 @@ client = commands.Bot(command_prefix = "!", intents=intents)
 gpt = Chat()
 
 
+
 @client.event
 async def on_ready():
    print("The bot is now ready for use") 
 
-testingServerID = 1197063979298725958
+testingServerID = 1200426642733027428
 
 
 # This is the example slash command
 #---------------------------------------------------------------------------------------------------------------------------------
 
-@client.slash_command(name = "hello",description="Replies with hello", guild_ids=[testingServerID]) # name has to be lowercase
-async def helloo(interaction: Interaction,text:str):
-    await interaction.response.send_message("Hello I am ready to do whatever you want me to do. I got this {}".format(text))
-
 @client.slash_command(name = "chat",description="Chat GPT in discord", guild_ids=[testingServerID]) # name has to be lowercase
 async def chat(interaction: Interaction,text:str):
     await interaction.response.send_message(gpt.read(text))
 
+@client.slash_command(name = "draw",description="DALLE in discord", guild_ids=[testingServerID]) # name has to be lowercase
+async def draw(interaction: Interaction,text:str):
+    await interaction.response.defer(ephemeral=True)
+    image_url = gpt.draw(text)
+    await interaction.followup.send(image_url)
+    #await interaction.response.send_message(image_url)
+
 #---------------------------------------------------------------------------------------------------------------------------------
 
+    
 
 
 
-client.run('MTIwNDk5NDg4OTkyMjY0NjA2Nw.G1BAFJ.S6e7KCsEH9hQvGvvqhWw0BWK6lhWeuHuLC66U8')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+client.run('MTIxMzY5NzQ0MDE0NjkxNTQyOA.GnR5Wz.zP0QaMXUwbOuoVw8iy0CSaY88BUkmJRwh1uvuQ')
